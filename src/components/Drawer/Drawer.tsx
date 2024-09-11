9944f628e297781eb279f1f264867402404e3e1d
Fernando Almeida <fernandoalmeid2@hotmail.com>
2024-09-11 14:34:44 -0400

    Improving the drawer component

--- src/components/Drawer/Drawer.tsx
+++ src/components/Drawer/Drawer.tsx
@@ -3,11 +3,10 @@ import './Drawer.css';
 
 interface DrawerProps {
   open: boolean;
-  onClose: () => void;
   onSelectTab: (index: number) => void;
 }
 
-const Drawer: React.FC<DrawerProps> = ({ open, onClose, onSelectTab }) => {
+const Drawer: React.FC<DrawerProps> = ({ open, onSelectTab }) => {
   return (
     <div className={`drawer ${open ? 'open' : ''}`}>
       <ul>
@@ -15,7 +14,6 @@ const Drawer: React.FC<DrawerProps> = ({ open, onClose, onSelectTab }) => {
         <li onClick={() => onSelectTab(1)}>Entrar</li>
         <li onClick={() => onSelectTab(2)}>Contato</li>
       </ul>
-      <button className="close-btn" onClick={onClose}>Close</button>
     </div>
   );
 };
