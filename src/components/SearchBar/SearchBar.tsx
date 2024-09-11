39206851ece05cc6c71693c5fccb2156ca315ce8
Fernando Almeida <fernandoalmeid2@hotmail.com>
2024-09-11 14:33:53 -0400

    adding searchBar component

---
+++ src/components/SearchBar/SearchBar.tsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import './SearchBar.css';
+
+interface SearchBarProps {
+  onSearch: (query: string) => void;
+}
+
+const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
+  const [query, setQuery] = useState('');
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value);
+    onSearch(event.target.value);
+  };
+
+  return (
+    <div className="search-bar">
+      <input
+        type="text"
+        value={query}
+        onChange={handleChange}
+        placeholder="Search menu items"
+      />
+    </div>
+  );
+};
+
+export default SearchBar;
