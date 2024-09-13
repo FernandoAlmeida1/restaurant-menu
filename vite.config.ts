d466c33d97d235c05f0f1b5c09bf967295102205
Fernando Almeida <fernandoalmeid2@hotmail.com>
2024-09-13 09:42:28 -0400

    Update Vite configuration and HTML for production

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,25 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  base: '/restaurant-menu/',
-  server: {
-    proxy: {
-      '/api/menu': {
-        target: 'https://cdn-dev.preoday.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api\/menu/, '/challenge/menu'),
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [react()],
+    base: command === 'build' ? '/restaurant-menu/' : '/',
+    server: {
+      proxy: {
+        '/api/menu': {
+          target: 'https://cdn-dev.preoday.com',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api\/menu/, '/challenge/menu'),
+        },
       },
     },
-  },
-  build: {
-    target: 'esnext',
-    outDir: 'dist',   
-    rollupOptions: {
-      input: 'src/main.tsx', 
-    }
-  }
+    build: {
+      target: 'esnext',
+      outDir: 'dist',
+      rollupOptions: {
+        input: 'src/main.tsx',
+      },
+    },
+  };
 });
