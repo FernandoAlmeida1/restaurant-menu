6ff0a8367b539e95075ec20999d938e02c23b578
Fernando Almeida <fernandoalmeid2@hotmail.com>
2024-09-12 20:33:20 -0400

    Adding test configurations

---
+++ jest.config.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jest-environment-jsdom',
+  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.jsx?$': 'babel-jest'
+  },
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
+  },
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect']
+};
