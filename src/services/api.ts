5d839c5c3383bd2e7dae5b880249369f3140791c
Fernando Almeida <fernandoalmeid2@hotmail.com>
2024-09-13 22:01:41 -0400

    Revert "fix: cors"
    This reverts commit 26b0cfbeea31b0910be9ac71043e720340661395.

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 export const fetchRestaurantDetails = async () => {
-  const response = await fetch("https://cdn-dev.preoday.com/challenge/venue/9", { mode: "no-cors" });
+  const response = await fetch("/api/challenge/venue/9");
   if (!response.ok) {
     const errorText = await response.text();
     throw new Error(`Failed to fetch restaurant details: ${response.status} ${errorText}`);
@@ -8,7 +8,7 @@ export const fetchRestaurantDetails = async () => {
 };
 
 export const fetchRestaurantMenu = async () => {
-  const response = await fetch("https://cdn-dev.preoday.com/challenge/menu", { mode: "no-cors" });
+  const response = await fetch("/api/challenge/menu");
   if (!response.ok) {
     const errorText = await response.text();
     throw new Error(`Failed to fetch menu details: ${response.status} ${errorText}`);
